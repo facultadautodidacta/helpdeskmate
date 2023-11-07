4b755ea95d9ec08c8786fce7b745c2b031a1f625
Facultad Autodidacta <roldan9091@gmail.com>
2023-11-07 12:00:50 -0600

    aspecto pimpeado

--- resources/views/modules/captura_soporte/index.blade.php
+++ resources/views/modules/captura_soporte/index.blade.php
@@ -3,8 +3,50 @@
 @section('contenido')
     <div class="container">
         <div class="row">
-            <div class="col">
-                <h2>Login de usuario</h2>
+            <div class="col mt-4">
+                <h2>Captura de trabajo de soporte</h2>
+                <a href="#" class="btn btn-primary">
+                    Agregar
+                </a>
+                <hr>
+                <table class="table table-sm table-hover">
+                    <thead>
+                        <tr>
+                            <th>Cliente</th>
+                            <th>Trabajo</th>
+                            <th>Monto</th>
+                            <th>Fecha</th>
+                            <th>Telefono</th>
+                            <th>Correo</th>
+                            <th>Trabajo futuro</th>
+                            <th>Posible fecha</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <a href="" class="btn btn-warning">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-danger">
+                                    <i class="fa-solid fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
--- resources/views/modules/listado_seguimiento/index.blade.php
+++ resources/views/modules/listado_seguimiento/index.blade.php
@@ -3,8 +3,37 @@
 @section('contenido')
     <div class="container">
         <div class="row">
-            <div class="col">
-                <h2>Login de usuario</h2>
+            <div class="col mt-4">
+                <h2>Listado de seguimiento</h2>
+                <hr>
+                <table class="table table-sm table-hover">
+                    <thead>
+                        <tr>
+                            <th>Cliente</th>
+                            <th>Trabajo</th>
+                            <th>Monto</th>
+                            <th>Fecha</th>
+                            <th>Telefono</th>
+                            <th>Correo</th>
+                            <th>Trabajo futuro</th>
+                            <th>Posible fecha</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            
+                        </tr>
+                    </tbody>
+                </table> 
             </div>
         </div>
     </div>
